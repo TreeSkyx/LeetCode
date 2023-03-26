38d1d7bb7d90a56172616294c49821ca0f797016
TreeSkyx <wiroon.som@gmail.com>
2023-03-27 00:28:08 +0700

    Add 2235

---
+++ 2235_AddTwoIntegers.py
@@ -0,0 +1,12 @@
+"""
+    2235. Add Two Integers
+    Given two integers num1 and num2, return the sum of the two integers.
+
+    :type num1: int
+    :type num2: int
+    :rtype: int
+"""
+class Solution(object):
+    def sum(self, num1, num2):
+        ans = num1+num2
+        return ans
