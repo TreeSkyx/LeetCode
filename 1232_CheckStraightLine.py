d1974d36094969d294e154e0f14c61294aaa6a8d
TreeSkyx <88649190+TreeSkyx@users.noreply.github.com>
2023-04-05 00:32:57 +0700

    1232 : add solution

---
+++ 1232_CheckStraightLine.py
@@ -0,0 +1,16 @@
+"""
+    1232. Check If It Is a Straight Line
+    GYou are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. 
+    Check if these points make a straight line in the XY plane.
+
+    :type coordinates: List[List[int]]
+    :rtype: bool
+
+    This following codes are modified for use in local editor. It may be slighly different from submission code.
+"""
+def checkStraightLine(coordinates):
+    (x1, y1), (x2, y2) = coordinates[0], coordinates[1]
+    for x3, y3 in coordinates[2:]:
+	    if (y2 - y1) * (x3 - x1) != (y3 - y1) * (x2 - x1):
+		    return False
+    return True
