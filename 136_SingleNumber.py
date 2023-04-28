57a8544da2c41c8f187921be81633f71ae19f164
TreeSkyx <88649190+TreeSkyx@users.noreply.github.com>
2023-04-28 10:58:55 +0700

    136 : add solution.

---
+++ 136_SingleNumber.py
@@ -0,0 +1,16 @@
+"""
+    136. Single Number
+    Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+    You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+    :type nums: List[int]
+    :rtype: int
+
+"""
+def singleNumber(nums):
+        uniqNum = 0
+        for i in nums:
+            uniqNum ^= i
+        return uniqNum
+
+print(singleNumber([4,1,2,1,2]))
