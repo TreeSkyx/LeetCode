15ff5726520475f8591ab841ce3c31a48bb45015
TreeSkyx <88649190+TreeSkyx@users.noreply.github.com>
2023-07-13 11:29:17 +0700

    50 : add solution.

---
+++ 50_Pow(x,n).py
@@ -0,0 +1,13 @@
+"""
+    50. Pow(x, n)
+    Implement pow(x, n), which calculates x raised to the power n (i.e., x^n).
+
+    :type x: float
+    :type n: int
+    :rtype: float
+
+"""
+def myPow(self, x, n):
+    return(pow(x,n))
+
+print(myPow("",2.10000,3))
