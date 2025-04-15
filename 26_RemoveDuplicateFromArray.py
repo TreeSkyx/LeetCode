fdd1893e48434d61e0a91980bf1d54bfe427ca93
Wiroon Somphaothong <88649190+TreeSkyx@users.noreply.github.com>
2025-04-15 19:10:20 +0700

    26 : add solution

--- 26_RemoveDuplicateFromArray.py
+++ 26_RemoveDuplicateFromArray.py
@@ -1,6 +1,10 @@
-def removeDuplicates(nums):
+def removeDuplicates(self, nums):
+    arr = []
     for i in range(len(nums)):
-        print(i)
+        if nums[i] not in arr:
+            arr.append(nums[i])
+    nums[:] = arr
+    return len(nums)
     
-removeDuplicates([1,1,2])
-
+print(removeDuplicates("", [1,1,2]));
+print(removeDuplicates("", [0,0,1,1,2,2,3,3,4]));
